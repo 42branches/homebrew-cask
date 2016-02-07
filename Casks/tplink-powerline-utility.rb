23239013d2fd534f8c95ccccfe4348eac4170a91
craig t smith <craig@42branches.co>
2016-02-07 16:36:42 +0000

    power line adaptor utility

---
+++ Casks/tplink-powerline-utility.rb
@@ -0,0 +1,9 @@
+cask 'tplink-powerline-utility' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://uk.tp-link.com/resources/software/TL-PA411KIT(EU_V2_MAC_Utility.zip'
+  name 'Powerline Utility'
+  homepage 'http://uk.tp-link.com/products/details/cat-18_TL-PA411KIT.html'
+  license :gratis
+end

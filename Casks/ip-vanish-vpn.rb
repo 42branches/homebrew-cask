c09272f096688a107db3bd24416b0c7d1c14901c
craig t smith <craig@42branches.co>
2016-02-07 16:36:33 +0000

    ip vanish

---
+++ Casks/ip-vanish-vpn.rb
@@ -0,0 +1,9 @@
+cask 'ip-vanish-vpn' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.ipvanish.com/software/osx/IPVanish.dmg'
+  name 'IPVanish'
+  homepage 'https://www.ipvanish.com/'
+  license :commercial
+end
